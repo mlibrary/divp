be52df1b34eda0017dae2c0cb6e9367c3442c362
Anthony Thomas <antmoth@umich.edu>
2025-12-10 10:54:57 -0500

    test Compressor instead of Compression

--- spec/compressor_spec.rb
+++ spec/compressor_spec.rb
@@ -1,9 +1,7 @@
-describe Compression do
+describe Compressor do
   before(:each) do
-    @config = Config.new({no_progress: true})
   end
   after(:each) do
-    TestShipment.remove_test_shipments
   end
   context "#run" do
     it "removes alpha when it exists" do
