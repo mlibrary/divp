06b722f96c68f961344758e0e3a5ebe4a78c9890
Monique Rio <mrio@umich.edu>
2025-12-15 12:13:21 -0500

    test remove_tiff_alpha in ImageMagick module

--- spec/compressor_spec.rb
+++ spec/compressor_spec.rb
@@ -3,9 +3,23 @@ describe Compressor do
 
   context "#run" do
     it "removes alpha when it exists" do
-      image_file = double("image_file", path: "spec/fixtures/10_10_8_400.tif")
+      image_file = double("image_file", path: "spec/fixtures/10_10_8_400_alpha.tif")
+      # image_file = double("image_file", path: "input/output-alpha2.tiff")
       compressor = Compressor.new(image_file: image_file, tmpdir: Pathname(temp_dir))
       expect(compressor).not_to be_nil
+    end
+  end
+end
+
+describe ImageMagick do
+  include_context "uses temp dir"
+  context "#remove_tiff_alpha" do
+    it "removes the alpha channel if it exists" do
+      tiff_path = "#{temp_dir}/input.tif"
+      FileUtils.copy("spec/fixtures/10_10_8_400_alpha.tif", tiff_path)
+      expect(TIFF.new(tiff_path).info[:alpha]).to eq(true)
+      ImageMagick.remove_tiff_alpha(tiff_path)
+      expect(TIFF.new(tiff_path).info[:alpha]).to eq(false)
     end
   end
 end
