d7aeec3bc73dd4144c65e148192583279fa6d422
Monique Rio <mrio@umich.edu>
2026-01-20 16:13:39 -0500

    handles deleting an existing checksum.md5 file before generating a new one

--- lib/stage/generate_checksums.rb
+++ lib/stage/generate_checksums.rb
@@ -16,7 +16,15 @@ class GenerateChecksums < Stage
     @bar.steps = shipment.objid_directories.count
     shipment.objid_directories.each_with_index do |dir, i|
       @bar.step! i, dir
+      remove_existing_checksum_file(dir)
       ChecksumFileGenerator.write(dir)
     end
+  end
+
+  private
+
+  def remove_existing_checksum_file(dir)
+    checksum_file_path = File.join(dir, "checksum.md5")
+    FileUtils.rm_f(checksum_file_path) if File.exist?(checksum_file_path)
   end
 end
--- spec/stage/generate_checksums_spec.rb
+++ spec/stage/generate_checksums_spec.rb
@@ -12,17 +12,33 @@ end
 
 describe GenerateChecksums do
   include_context "uses temp dir"
-  it "does something" do
-    barcode = "39015002231713"
-    shipment_path = "#{temp_dir_path}/test_shipment"
-    item_path = "#{shipment_path}/#{barcode}"
+  let(:barcode) { "39015002231713" }
+  let(:shipment_path) { "#{temp_dir_path}/test_shipment" }
+  let(:item_path) { "#{shipment_path}/#{barcode}" }
+  let(:shipment) { Shipment.new(shipment_path) }
+  subject do
+    described_class.new(shipment, config: Config.new({no_progress: true}))
+  end
+
+  def set_up_shipment
     FileUtils.mkdir_p(item_path)
     FileUtils.cp("spec/fixtures/10_10_8_400.jp2", item_path)
+  end
 
-    shipment = Shipment.new(shipment_path)
-    stage = described_class.new(shipment)
-    stage.run!
+  it "creates a checksum file" do
+    set_up_shipment
+
+    subject.run!
     checksum_contents = File.read(File.join(item_path, "checksum.md5"))
     expect(checksum_contents).to include("  10_10_8_400.jp2")
+  end
+
+  it "original checksum file is removed before calculating checksum" do
+    set_up_shipment
+    FileUtils.touch("#{item_path}/checksum.md5")
+    subject.run!
+    checksum_contents = File.read(File.join(item_path, "checksum.md5"))
+    expect(checksum_contents).to include("  10_10_8_400.jp2")
+    expect(checksum_contents).not_to include("  checksum.md5")
   end
 end
