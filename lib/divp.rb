8ff778d23f28e6b4554a0a054d73006ed8a10c13
Anthony Thomas <antmoth@umich.edu>
2026-02-27 10:54:29 -0500

    Raise error when no shipment directory
    (without crashing bundle)

--- lib/divp.rb
+++ lib/divp.rb
@@ -11,16 +11,22 @@ require 'thor'
 
 module DIVP 
   class CLI < Thor
-    desc 'process', 'process'
-    option :config_profile, :aliases => ['c'], :desc => 'Configuration PROFILE (e.g., "dlxs" loads config.dlxs.yaml; optional)', :default => nil
-    option :config_dir, :aliases => ['d'], :desc => 'Configuration directory DIRECTORY (optional)', :default => nil
+    desc 'process SHIPMENT_DIRECTORY', 'process'
+    option :config_profile, :aliases => ['c'], :desc => 'Configuration PROFILE (e.g., "dlxs" loads config.dlxs.yaml)'
+    option :config_dir, :aliases => ['d'], :desc => 'Configuration directory DIRECTORY'
+    option :help, :aliases => ['h'], :desc => 'Try "divp help process"'
     option :restart_all, :aliases => ['R'], :desc => 'Discard status.json and restart all stages', :type => :boolean
     option :verbose, :aliases => ['v'], :desc => 'Run verbosely'
     option :tagger_scanner, :banner => 'SCANNER', :desc => 'Set scanner tag to SCANNER'
     option :tagger_software, :banner => 'SOFTWARE', :desc => 'Set scan software tag to SOFTWARE'
     option :tagger_artist, :banner => 'ARTIST', :desc => 'Set artist tag to ARTIST'
-    def process
-      puts options[:config_profile]
+
+    def process(*shipment_directory)
+      if shipment_directory.empty?
+        raise Thor::RequiredArgumentMissingError, 'Missing required parameter SHIPMENT_DIRECTORY'.red
+      end
+      puts options
+      puts shipment_directory
     end
 
     def self.exit_on_failure?
