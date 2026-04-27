272e70e2223653d4a181621e8a6f738d063113e9
Monique Rio <mrio@umich.edu>
2026-04-27 11:38:17 -0400

    remove lazy assignment of image files because the ones in the main folder can change

--- lib/item.rb
+++ lib/item.rb
@@ -22,8 +22,9 @@ class Item
     )
   end
 
+  # These can change so we can't memoize them
   def image_files
-    @image_files = (Dir.children(@path) || []).filter_map do |child|
+    (Dir.children(@path) || []).filter_map do |child|
       if valid_file_types.include?(ImageFile.file_type(child))
         file_path = File.join(@path, child)
         objid_file = File.join(objid_components, child)
